8dc831c9bd72019f850ce19f90f18565fa77df2c
IvanLabza <vanalabza77@gmail.com>
2024-04-07 15:39:23 +0300

    main

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialContacts = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
+
+const tasksSlice = createSlice({
+  name: "tasks",
+  initialState: initialContacts,
+  reducers: {
+    addTContact: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(text) {
+        return {
+          payload: {
+            text,
+            id: nanoid(),
+          },
+        };
+      },
+    },
+    deleteContact(state, action) {
+      const index = state.findIndex((task) => task.id === action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
+export const { ContactTask, deleteContact, toggleCompleted } = tasksSlice.actions;
+export const tasksReducer = tasksSlice.reducer;
